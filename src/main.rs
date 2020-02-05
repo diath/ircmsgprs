2285631d41112d68cfd6cb175fcbd68b4922c130
Kamil Chojnowski <contact@diath.net>
2020-02-05 04:11:14 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod parser;
+
+fn main() {
+	let mut parser = parser::Parser::new("PING :1337\r\n");
+	let _result = parser.parse();
+}
---
+++ src/parser.rs
@@ -0,0 +1,63 @@
+use std::str::Chars;
+use std::iter::Peekable;
+
+#[derive(Default)]
+pub struct Message {
+    pub prefix: String,
+    pub command: String,
+    pub params: String,
+}
+
+pub struct Parser<'a> {
+    data: Peekable<Chars<'a>>,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(line: &str) -> Parser {
+        Parser { data: line.chars().peekable() }
+    }
+
+    pub fn parse(&mut self) -> Option<Message> {
+        // message = [ ":" prefix SPACE ] command [ params ] crlf
+        let mut message = Message {
+            ..Default::default()
+        };
+
+        let chr = self.data.peek()?;
+        if *chr == ':' && message.prefix.len() == 0 {
+            if let Some(prefix) = self.parse_prefix() {
+                message.prefix = prefix;
+            } else {
+                return None;
+            }
+        }
+
+        if message.command.len() == 0 {
+            if let Some(command) = self.parse_command() {
+                message.command = command;
+            } else {
+                return None;
+            }
+        }
+
+        if let Some(params) = self.parse_params() {
+            message.params = params;
+        } else {
+            return None;
+        }
+
+        Some(message)
+    }
+
+    fn parse_prefix(&mut self) -> Option<String> {
+        None
+    }
+
+    fn parse_command(&mut self) -> Option<String> {
+        None
+    }
+
+    fn parse_params(&mut self) -> Option<String> {
+        None
+    }
+}
